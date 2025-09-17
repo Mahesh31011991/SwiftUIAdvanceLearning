b2ace97609050c81e5b5d64fa670c76f618a9aea
Mahesh Behere <maheshbehere14@gmail.com>
2025-09-17 15:54:16 +0530

    Initial Commit

---
+++ SwiftUIAdvanceLearning/SwiftUIAdvanceLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvanceLearningApp.swift
+//  SwiftUIAdvanceLearning
+//
+//  Created by Mahesh Behere on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvanceLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
